b2f0951ca199a5dfdecaa39fcb4dc676444cca29
Tobias <tobias.bp2004@gmail.com>
2025-11-10 17:13:29 -0300

    feat: auth

--- src/types/Schemas/shirt.schema.ts
+++ src/types/Schemas/shirt.schema.ts
@@ -1,16 +1,15 @@
 import { z } from 'zod'
 
 export const ShirtSchema = z.object({
-  name: z.string(),
-  brand: z.string(),
-  size: z.string(),
-  price: z.number(),
-  stock: z.number(),
-  color: z.string(),
-  category: z.string(),
-  material: z.string(),
-  gender: z.string(),
+	name: z.string(),
+	brand: z.string(),
+	size: z.string(),
+	price: z.number(),
+	stock: z.number(),
+	color: z.string(),
+	category: z.string(),
+	material: z.string(),
+	gender: z.string(),
 })
-
 
 export type Shirt = z.infer<typeof ShirtSchema>
